bab9b48283a5481c3c41ecf5a43f6356f83e593a
gui18br <guilhermenovaislimaa@hotmail.com>
2023-10-04 13:32:59 -0300

    Add initial login interface.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     
+      <div className="container">
+        <form>
+          <p>Seja bem vindo</p>
+          <h1>Faça o login na sua conta</h1>
+          <label>Email</label>
+          <input type="email" />
+          <label>Senha</label>
+          <input type="password" />
+          <button>Entrar</button>
+        </form>
+      </div>
     </div>
   );
 }
